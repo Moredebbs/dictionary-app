ad7143847d819c360f082814ce5c94286add9302
Moredebbs <90006497+Moredebbs@users.noreply.github.com>
2025-01-04 23:07:13 +0000

    added footer, logo, search box and usestate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import "./App.css";
-
-function App() {
+import Dictionary from "./Dictionary";
+import logo from "./logo.png";
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">holla</header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="dico-logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <small>
+          <footer className="App-footer">
+            This project was coded by
+            <a
+              href="https://github.com/Moredebbs"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              Moyinoluwa Owoeye
+            </a>{" "}
+            is{" "}
+            <a
+              href="https://github.com/Moredebbs/react-weather-app-project"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on Github
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://react-mi-clima.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              hosted on Netlify
+            </a>
+          </footer>
+        </small>
+      </div>
     </div>
   );
 }
-
-export default App;
