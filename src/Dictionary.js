a0db17783cbedc60b8d6a86562506b311bd2e90f
Moredebbs <90006497+Moredebbs@users.noreply.github.com>
2025-01-09 15:46:03 +0000

    added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
   function search(event) {
     event.preventDefault();
     alert(`searching for ${keyword} definition`);
+    let apiKey = "80428f51a21b4t3dfe9d3b547of6cb3f";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?${keyword}=sunset&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
