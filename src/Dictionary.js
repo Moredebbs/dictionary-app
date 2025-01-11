da58c6460a18b38961c4ac212ba6e504a9480e4b
Moredebbs <90006497+Moredebbs@users.noreply.github.com>
2025-01-11 23:17:35 +0000

    added images to the app

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,28 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
+  }
+  function handleImgResponse(response) {
+    console.log(response);
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+    let apiKey = "80428f51a21b4t3dfe9d3b547of6cb3f";
+    let imgApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;
+    axios.get(imgApiUrl).then(handleImgResponse);
   }
 
   function handleSubmit(event) {
@@ -45,6 +54,7 @@ export default function Dictionary(props) {
           </form>
           <div className="hint">i.e. paris, sunset, wine , forest, gifts</div>
           <Results results={results} />
+          <Photos photos={photos} />
         </section>
       </div>
     );
